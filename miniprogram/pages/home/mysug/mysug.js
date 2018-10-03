efe5246ac2dd8c60d7115a853b6f9b47aa2c0ec4
lcs <lcs1998@vip.qq.com>
2018-10-03 11:52:35 +0800

    修复我的评论，我的发表bug

--- miniprogram/pages/home/mysug/mysug.js
+++ miniprogram/pages/home/mysug/mysug.js
@@ -8,78 +8,72 @@ Page({
    * 页面的初始数据
    */
   data: {
-    rows:[],
+    rows: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (e) {
+  onLoad: function(e) {
     const _ = app.globalData.db.command
     app.globalData.db.collection('qyzx_texts').where({
       // gt 方法用于指定一个 "大于" 条件，此处 _.eq()是获取与其相等的项
       _openid: _.eq(app.globalData.userInfo._openid),
       deleted: false
     })
-      .get({
-        success: function (res) {
-          console.log("查询结果: ", res.data);
-          this.setData({
-            rows: res.data
-          });
-          // console.log("rows的值为：", this.data.rows)
-        }
+      .get().then(res => {
+        this.setData({
+          rows: res.data,
+        });
       })
-
-    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
