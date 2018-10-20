9013008e4ce082f65fb8e2e42eba10e2caccdf32
lcs <lcs1998@vip.qq.com>
2018-10-20 21:56:45 +0800

    教师页触底刷新

--- miniprogram/pages/contact/index.js
+++ miniprogram/pages/contact/index.js
@@ -1,6 +1,7 @@
 //index.js
 //获取应用实例
 const db = wx.cloud.database();
+var idd = []
 
 Page({
   bind_tal: function(e) {
@@ -51,6 +52,26 @@ Page({
       }
     })
   },
+
+  onReachBottom: function () {
+    console.log("onReachBottom函数运行！")
+    const _ = db.command
+    db.collection("teacher_info").where({
+      name: _.nin(idd)
+    }).get().then(res => {
+        console.log(res)
+        for (var index in res.data) {
+          idd.push(res.data[index].name)
+        }
+        console.log("id:", idd)
+        this.setData({
+          teacher_info: this.data.teacher_info.concat(res.data)
+        })
+      })
+
+  },
+
+
   onLoad: function(options) {
     // 生命周期函数--监听页面加载
     showView: (options.showView == "true" ? true : false)
@@ -60,6 +81,11 @@ Page({
         this.setData({
           teacher_info:res.data
         })
+        for (var index in res.data) {
+          console.log("index:", res.data[index].name)
+          idd[index] = res.data[index].name
+        };
+        console.log("ID数组为：", idd)
       })
   },
   onChangeShowState: function() {
